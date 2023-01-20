f8f9b003d094342f1c158825ec6f96663c8ce3f3
El Ghazali <agep2024@gmail.com>
2023-01-20 11:54:35 +0000

    20012023_1154

--- lib/naghmat.dart
+++ lib/naghmat.dart
@@ -1,6 +1,5 @@
-
-
 import 'package:flutter/material.dart';
+import 'package:audioplayers/audioplayers.dart';
 
 class Naghmat extends StatefulWidget {
   const Naghmat({super.key});
@@ -9,12 +8,38 @@ class Naghmat extends StatefulWidget {
   State<Naghmat> createState() => _NaghmatState();
 }
 
+//6422561867 - 8001785693 -
 class _NaghmatState extends State<Naghmat> {
+  final player = AudioPlayer();
+  TextButton getButton({int numMusic = 1, String titleText = ''}) {
+    return TextButton(
+      onPressed: () async {
+        await player.play(DeviceFileSource('assets/$numMusic.mp3'));
+      },
+      child: Text(
+        titleText,
+        style: TextStyle(
+          fontSize: 20,
+          fontFamily: 'Amiri',
+          fontWeight: FontWeight.bold,
+        ),
+      ),
+    );
+  }
+
+  List<TextButton> bottuns = [];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('نغماتي'),),
-      body: Container(),
+      appBar: AppBar(
+        title: Text('نغماتي'),
+      ),
+      body: Container(
+        child: Column(
+          children: bottuns,
+        ),
+      ),
     );
   }
 }
